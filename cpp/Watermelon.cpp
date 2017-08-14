afe0ca80fb216b620672e5a1312ecd55b6644946
evalir.io <hi.evalir@gmail.com>
2017-08-14 12:06:16 -0400

    Add watermelon solution

---
+++ cpp/Watermelon.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+int x = 0;
+std::string result = "a";
+
+//Get number
+std::cin >> x;
+
+//Print output depending on type
+if (x == 2) {
+    std::cout << "NO" << std:: endl;
+} else if (x % 2 == 0) {
+    std::cout << "YES" << std::endl;
+} else {
+    std::cout << "NO" << std::endl;
+}
+
+return 0;
+
+}
