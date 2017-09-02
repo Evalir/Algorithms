4d6e5fe6458bac6dde4239b0f521808e0944b7b3
evalir.io <hi.evalir@gmail.com>
2017-09-02 13:23:13 -0400

    Clean solution for problems and include A339

---
+++ cpp/A339.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+  vector<char> numbers;
+  string addition;
+
+  cin >> addition;
+
+  for (int i = 0; i < addition.length(); i++) {
+    if (addition[i] == '1' || addition[i] == '2' || addition[i] == '3') {
+      numbers.push_back(addition[i]);
+    }
+  }
+
+  sort(numbers.begin(), numbers.end());
+  addition = "";
+
+  for (int i = 0; i < numbers.size(); i++) {
+    cout << numbers[i];
+    if(i < numbers.size() - 1 ) cout << "+";
+    if(i == numbers.size()) cout << "\n";
+  //  addition += string(1, numbers[i]);
+  //if(i < numbers.size() - 1 ) addition += "+";
+  }
+
+
+  //cout << addition << endl;
+}
