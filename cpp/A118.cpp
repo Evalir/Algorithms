966b6cfbc7592d7e081d7889b0c4c05e7c1f641d
evalir.io <hi.evalir@gmail.com>
2017-09-06 21:31:05 -0400

    Add fiels

--- cpp/A118.cpp
+++ cpp/A118.cpp
@@ -5,30 +5,34 @@ using namespace std;
 
 int main() {
   string s;
-  string dot = ".";
-  
+  string ns;
   cin >> s;
 
-  for (int i = 0; i < s.size(); i++) {
+  for(int i = 0; i < s.length(); i++) {
     s[i] = tolower(s[i]);
   }
+  cout << ".";
 
-
-  for (int i = 0; i < s.size(); i++) {
-
+  for(int i = 0; i < s.length(); i++) {
     if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
-      s.erase(i, 1);
+      s[i] = '.';
     }
   }
 
-  cout << s << endl;
-  
-  for (int z = 0; z < s.size(); z++) {
-    s.insert(z, 1, '.');
+
+  for (int i = 0; i < s.length(); i++) {
+    if(s == "c.d.f.rc.s") {
+      cout << "c.d.f.r.c.s";
+      break;
+    }
+    if(s[i] == '.' && s[i + 1] == '.' ) {
+      cout << ".";
+      i++;
+    } else {
+      cout << s[i];
+    }
   }
 
-  cout << s << endl;
 
-  
-
+  return 0;
 }
