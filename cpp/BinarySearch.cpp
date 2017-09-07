43671bcee40d0ae1ec2b8aa198fac1b3e54721ff
evalir.io <hi.evalir@gmail.com>
2017-09-07 13:56:28 -0400

    Add Binary Search Array

---
+++ cpp/BinarySearch.cpp
@@ -0,0 +1,33 @@
+//Binary Search algorithm
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int bSearch(vector<int> arr, int goal){
+  int max = arr.size() - 1;
+  int min = 0;
+  int guess = 0;
+  while(min <= max) {
+    guess = (max + min) / 2;
+    if(arr[guess] == goal) {
+      return guess;
+    } else if (arr[guess] < goal) {
+      min = guess + 1;
+    } else if (arr[guess] > goal) {
+      max = guess - 1;
+    }
+  }
+
+  cout << "Number not found in array" << endl;
+  return -1;
+
+}
+
+int main() {
+  vector<int> arr;
+  for (int i = 0; i < 10; i++) {
+    arr.push_back(i);
+    cout << arr[i] << endl;
+  }
+  cout << bSearch(arr, 7) << endl;
+}
