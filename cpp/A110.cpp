ebb41c03d99eb60ccdc302868048af4055193e79
evalir.io <hi.evalir@gmail.com>
2017-09-20 23:35:14 -0400

    Add solutions for problems

---
+++ cpp/A110.cpp
@@ -0,0 +1,17 @@
+//Problem A110 from codeforces
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  long long n;
+  long nlucky;
+
+  cin >> n;
+
+ for (int i = 0; i < n.length(); i++) {
+    if (n[i] == 4 || n[i] == 7) nlucky++;
+ }
+
+  if(nlucky == ) 
+}
--- cpp/A131.cpp
+++ cpp/A131.cpp
@@ -1,35 +1,21 @@
-//Problem A131 from codeforces
-//By evalir.io
+//problem 131A from codeforces
 
 #include <iostream>
-#include <string>
-#include <algorithm>
-
+#include <vector>
 using namespace std;
 
-int main(){
-    string caps;
-    int counter = 0;
-
-    cin >> caps;
-
-    for(int i = 0; i < caps.length(); i++) {
-      if (caps[i] > 90 && i != 0) counter - 2;
-      else if (caps[i] >= 65 && caps[i] <= 90) counter++;
-    }
-
-    if( (counter == caps.length() - 1 || counter == caps.length()) {
-      for (int i = 0; i < caps.length(); i++) {
-
-        if (caps[i] >= 65 && caps[i] <= 90) caps[i] = tolower(caps[i]);
-        if (caps[i] > 90) caps[i] = toupper(caps[i]);
-
-      }
-    }
-
-    cout << caps << endl;
-
-
-
-    return 0;
+int main() {
+	vector<int> lucky = {4,7,44,77,47,74,447,474,744,444,777, 477};
+	int number;
+	cin >> number;
+	for(int i = 0; i < lucky.size(); i++) {
+	    if (number % lucky[i] == 0) {
+	        cout << "YES";
+	        //cout << lucky[i];
+	        return 0;
+	    }
+	}
+	cout << "NO" << endl;
+	
+	return 0;
 }
