cec24820c96acc2e7649f5b420351032f9816eb2
evalir.io <hi.evalir@gmail.com>
2017-09-23 01:40:19 -0400

    Add solutions, to-do A337

---
+++ cpp/A337.cpp
@@ -0,0 +1,44 @@
+//Problem A337 from codeforces
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  int n, m; //n of students and puzzles
+  int piece;
+  int high, low;
+  int diff = 0;
+  vector<int> pieces;
+  vector<int> subset;
+  
+  cin >> n >> m;
+  for (int i = 0; i < m; i++) {
+    cin >> piece;
+    pieces.push_back(piece);
+  }
+
+  for (int i = 0; i < n; i++) {
+    int x = 0;
+    int y = n;
+    int currentdiff = 0;
+    for(x; x < y; x++) {
+      subset.push_back(pieces[x]);
+    } 
+    sort(subset.begin(), subset.end(), greater<int>());
+
+    high = subset[0];
+    low = subset[subset.size() - 1];
+    currentdiff = high - low;
+
+    if (currentdiff < diff) {
+      diff = currentdiff;
+      x++;
+      y++;
+    } else {}
+
+  }
+
+  cout << diff << endl;
+}
