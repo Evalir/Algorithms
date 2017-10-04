0f514b31b15ab2ce5ec56b42dde9613e7ca55a0d
evalir.io <hi.evalir@gmail.com>
2017-10-04 09:08:14 -0400

    Add preliminar solution

--- cpp/A486.cpp
+++ cpp/A486.cpp
@@ -10,9 +10,18 @@ int main() {
     long long n;
     cin >> n;
     long long func = 0;
-    long long even;
-    long long odd;
-    for (long long i = 0; i < n; i+= 2) {
+    long long even = 0;
+    long long odd = 1;
+
+//    func = even - odd;
+
+    for (long long i = 0; i < n / 2; i++) {
+        even += i;
+    }
+    even = even*2;
+    if ( n % 2 != 0) even++;
+
+    for (long long i = 0; i < n / 2; i+= 2) {
 
     }
 
@@ -28,3 +37,13 @@ int main() {
 
     cout << func << endl;
 }
+/*BREAKING DOWN THE PROBLEM:
+ *  We need to get the function -1+2-3+...+(-1)to the n * n.
+ *  Odd numbers are subtracted and even numbers are added.
+ *  We can cut the even numbers computation by half by summing all numbers up to the middle and multiplying
+ *  by two.
+ *  The odd numbers have no easy way of getting cut by half.
+ *
+ *
+ *
+ * */
