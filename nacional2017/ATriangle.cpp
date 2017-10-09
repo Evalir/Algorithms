ab5846f71805c72bb15bfb75d97eccde223e81ac
evalir.io <hi.evalir@gmail.com>
2017-10-08 20:48:26 -0400

    More organization and problem solving

---
+++ nacional2017/ATriangle.cpp
@@ -0,0 +1,16 @@
+// Seen in ACM-ICPC nationals 2017 7/10/17
+// Created by Enrique Ortiz on 10/7/17.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    int b = 2*a;
+int Atriangle = (b*5)/2;
+int Arectangle = b*a;
+    cout << Atriangle + Arectangle << endl;
+}
+//Que jodio diparate.
