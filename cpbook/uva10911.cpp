8b169048a23ecf10c1c782d33c734ba842756e5c
evalir.io <hi.evalir@gmail.com>
2017-10-12 08:50:06 -0400

    Add uva problem with no solution cuz it requires DP & bitmasking lol

---
+++ cpbook/uva10911.cpp
@@ -0,0 +1,13 @@
+/* Problem 10911 from uva online judge
+   seen in book "competitive programming", page 1
+*/
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main() {
+
+
+}
