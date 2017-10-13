07c3b6917a7fc30ee73c0d9a2122ee13a91b1b72
evalir.io <hi.evalir@gmail.com>
2017-10-13 18:11:14 -0400

    Organize files, add folder for problems and one for learning material

---
+++ Problems/edcontest2017/Achores.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, k, x;
+    vector<int> chores;
+    cin >> n >> k >> x;
+
+    for(int i = 0; i < n; i++){
+	int ch;
+	cin >> ch;
+	chores.push_back(ch);
+    }
+    
+    sort(chores.begin(), chores.end(), greater<int>());
+    
+    for(int i = 0; i < k; i++) 
+	if (chores[i] > x) chores[i] = x;
+    
+//    for (int i = 0; i < chores.size(); i++)
+//	cout << chores[i] << endl;
+    
+    int sum = 0;
+    for(int i = 0; i < chores.size(); i++)
+	sum += chores[i];
+
+    
+    cout << sum << endl;
+    
+    return 0;
+}
