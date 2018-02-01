5bca84ab39d7a248025a9abe1ee36d8da8b31163
evalir.io <hi.evalir@gmail.com>
2018-01-31 23:27:11 -0400

    Add files

---
+++ Problems/SPOJ/test.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x = 0;
+    scanf("%d", &x);
+
+    if(x > 100) {
+        printf("Loco eso es muy grande");
+    }
+    else {
+        printf("okay");
+    }
+
+    return 0;
+}
