8d51f0dbb7597ea0db9a24615b0af715329b166c
evalir.io <hi.evalir@gmail.com>
2018-03-05 20:09:46 -0400

    Add solutions

--- Problems/codeforces/C580.cpp
+++ Problems/codeforces/C580.cpp
@@ -1,73 +1,113 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <cmath>
-#include <climits>
 #include <algorithm>
-#include <bitset>
-#include <list>
-#include <string>
-#include <sstream>
-#define INF (int)1e9
-#define pb push_back
-#define EPS 1e-9
-#define all(a) (a).begin(), (a).end()
-#define PI 3.1415926535897932384626
-#define READALL(vec) for(int i = 0; i < N; i++){int x; cin >> x; (vec).pb(x);}
+#include <stack>
+#include <vector>
+#include <cmath>
+ 
 using namespace std;
-typedef long long llong;
-typedef unsigned long long ull;
-typedef vector<int> VI;
-typedef vector<VI> VII;
-typedef vector<long long> VLL;
-typedef pair<int, int> ii;
-typedef map<int,int> FREQ;
-int n, m;
+ 
+struct Edge {
+   int from;
+   int to;
+   int cost;
+   bool hasCat;
+};
 
-int main(){
-    cin >> n >> m;
-    int graph[n][n];
-    vector<bool> cats(n, 0);
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n ; j++) {
-            graph[i][j] = 0;
-        }
-    }
+typedef vector<Edge> VE;
+typedef vector<VE> VVE;
+ 
+int N, M;
+VVE adj;
+vector<bool> cats;
+stack<int> lastVisit;
+int catCount = 0;
+int canGo = 0;
+vector<bool> visited;
 
-    for(int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        cats[i] = x;
-    }
-
-    for(int i = 0; i < n-1; i++) {
-        int a, b;
-        cin >> a >> b;
-        a--; 
-        b--;
-        graph[a][b] = 1;
-        graph[b][a] = 1;
-    }
-
-    // for(int i = 0; i < n; i++) {
-    //     if (cats[i] == 0) {
-    //         cout << "VERTEX " << i << " HAS NO CATS" << endl;
-    //     }
-    //     else {
-    //         cout << "VERTEX " << i << " HAS A CAT" << endl;
-    //     }
-    // }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << graph[i][j] << " NODE " << i+1 << ' ' << j+1 << endl;
-        }
-        cout << endl;
-    }
-
-
-    return 0;
+void printGraph() {
+    for (int u = 0; u < N; u++) {
+      cout << "Node " << u+1 << " has edges: ";
+      for (Edge e : adj[u]) {
+         cout << "(" << e.from+1 << "," << e.to+1 << "," << "NO COST" << ") ";
+      }
+      cout << endl;
+   }
 }
+
+void dfs(int u) {
+   visited[u] = true;
+   for (Edge e : adj[u]) {
+       //cerr << "VISITING EDGE " << e.to+1 << " FROM " << e.from+1 << " WITH CAT COUNT"<< catCount << endl;
+       if (cats[e.to]) {
+           catCount++;
+           //cout << "COUNTED CAT AT EDGE " << e.from+1 << endl;
+           if (catCount > M) {
+            //cout << "CANNOT TRAVERSE, TOO MANY MEOWS" << endl;
+            catCount = 0;
+            break;
+           }
+        }
+       if (!visited[e.to]) {
+          // e.to es un hijo de u
+          dfs(e.to);
+       }
+       else if (visited[e.to] && adj[u].size() == 1) {
+          if (!cats[e.from]) { //Currently at leaf, ++ on restaurants and reset catCount
+            //cout << "RESETING COUNT ON LEAF " << e.from+1 << endl;
+            canGo++; 
+            catCount = 0;
+            break;
+         }
+       }
+   }
+}
+ 
+void dfs_all() {
+   for (int u = 0; u < N; u++) {
+      if (!visited[u]) {
+        //cerr << "ENTERING COMPONENT STARTING AT " << u+1 << endl;
+        if (cats[u]) catCount++;
+        dfs(u);
+         //cout << comp_size << endl;
+      }
+   }
+}
+ 
+int main() {
+ 
+   cin >> N >> M;
+   adj = VVE(N);
+
+    for(int i = 0; i < N; i++) {
+       bool x;
+       cin >> x;
+       cats.push_back(x);
+   }
+
+    for(int i = 0; i < N-1; i++) {
+        int u, v, cost = 0;
+        cin >> u >> v;
+        --u, --v;
+        adj[u].push_back({u, v, cost});
+        adj[v].push_back({v, u, cost});    // Undirected graph
+   }
+
+    //start DFS
+    visited = vector<bool>(N, false);
+    dfs_all();
+    //printGraph();
+    cout << canGo << endl;
+ 
+}
+
+/*
+7 1
+1 0 1 1 0 0 0
+1 2
+1 3
+2 4
+2 5
+3 6
+3 7
+
+*/
--- learnCpp/DFSJoa.cpp
+++ learnCpp/DFSJoa.cpp
@@ -16,12 +16,13 @@ typedef vector<VE> VVE;
 int N;
 VVE adj;
  
-int comp_size;
+int comp_size; //holds connected component size
 vector<bool> visited;
 void dfs(int u) {
    visited[u] = true;
    comp_size++;
    for (Edge e : adj[u]) {
+       cerr << "VISITING EDGE " << e.to+1 << " FROM " << e.from+1 << endl;
        if (!visited[e.to]) {
           // e.to es un hijo de u
           dfs(e.to);
@@ -32,17 +33,16 @@ void dfs(int u) {
    }
 }
  
- 
 void dfs_all() {
    for (int u = 0; u < N; u++) {
       if (!visited[u]) {
-         comp_size = 0;
-         dfs(u);
-         cout << comp_size << endl;
+        cout << "ENTERING COMPONENT STARTING AT " << u << endl;
+        comp_size = 0;
+        dfs(u);
+         //cout << comp_size << endl;
       }
    }
 }
- 
  
 int main() {
  
@@ -55,6 +55,8 @@ int main() {
    for (int m = 0; m < cantEjes; m++) {
       int u, v, cost = 0;
       cin >> u >> v; //>> cost;
+      u--;
+      v--;
       adj[u].push_back({u, v, cost});
       adj[v].push_back({v, u, cost});    // para grafo undirected
    }
@@ -63,7 +65,7 @@ int main() {
    for (int u = 0; u < N; u++) {
       cout << "Vertice " << u << " tiene ejes: ";
       for (Edge e : adj[u]) {
-         cout << "(" << e.from << "," << e.to << "," << e.cost << ") ";
+         cout << "(" << e.from << "," << e.to << "," << "NO COST" << ") ";
       }
       cout << endl;
    }
