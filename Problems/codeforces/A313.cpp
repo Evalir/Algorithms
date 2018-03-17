9ba37e0684c66432b08c13023b88115d2b07ae64
evalir.io <hi.evalir@gmail.com>
2018-03-17 16:02:31 -0400

    Add solutiosn for bank account and vile grasshoppers

--- Problems/codeforces/A313.cpp
+++ Problems/codeforces/A313.cpp
@@ -5,37 +5,46 @@
 #include <stack>
 #include <queue>
 #include <cmath>
+#include <climits>
 #include <algorithm>
 #include <bitset>
 #include <list>
+#include <string>
+#include <sstream>
 #define INF (int)1e9
+#define pb push_back
+#define EPS 1e-9
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626
+#define READALL(vec) for(int i = 0; i < N; i++){int x; cin >> x; (vec).pb(x);}
 using namespace std;
 typedef long long llong;
-typedef unsigned long long uint64;
+typedef unsigned long long ull;
 typedef vector<int> VI;
 typedef vector<VI> VII;
- 
- 
+typedef vector<long long> VLL;
+typedef pair<int, int> ii;
+typedef vector<vector<ii> > Graph;
+typedef map<int,int> FREQ;
+
 int main(){
-    string S;
-    cin >> S;
+	string acc;
+	cin >> acc;
+	
+	if (acc[0] != '-') {
+		cout << acc << endl;
+		return 0;
+	}
+	else if (acc[0] == '-') {
+		char ld = acc.back();
+		acc.pop_back();
+		char ld2 = acc.back();
+		acc.pop_back();
+		acc.push_back(min(ld, ld2));
+		//cerr << min(ld, ld2) << endl;
+		if (acc[0] == '-' && acc[1] == '0')acc.erase(0,1);
+		cout << acc << endl;
+		return 0;
+	}
 
-    if (S[0] == '-') {
-        if (S[S.size()-1] > S[S.size()-2]) {
-            S.erase(S.size()-1, 1);
-            if (S == "-0") cout << "0" << endl;
-            else cout << S << endl;
-        }
-        else {
-            S.erase(S.size()-2, 1);
-            if (S == "-0") cout << "0" << endl;
-            else cout << S << endl;
-        }
-    } else {
-        cout << S << endl;
-        return 0;
-    }
-
-
-    return 0;
 }
--- Problems/codeforces/B937.cpp
+++ Problems/codeforces/B937.cpp
@@ -1,82 +1,62 @@
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <cmath>
-#include <climits>
 #include <algorithm>
-#include <bitset>
-#include <list>
-#include <string>
-#include <sstream>
-#define INF (int)1e9
-#define pb push_back
-#define EPS 1e-9
-#define all(a) (a).begin(), (a).end()
-#define PI 3.1415926535897932384626
-#define READALL(vec) for(int i = 0; i < N; i++){int x; cin >> x; (vec).pb(x);}
+#include <cmath>
 using namespace std;
-typedef long long llong;
-typedef unsigned long long ull;
-typedef vector<int> VI;
-typedef vector<VI> VII;
-typedef vector<long long> VLL;
-typedef pair<int, int> ii;
-typedef vector<vector<ii> > Graph;
-typedef map<int,int> FREQ;
 
-int main(){
-    int p, n;
-    cin >> p >> n;
-	bool isPrime = false;
-	if (n > 300 && p > 300) {
-    for(int k = n-1; k >= n-1-300; k-= 2) {
-		//cerr << "Trying with " << k << endl;
-		if (k <= p) {
-			cout << -1 << endl;
-			return 0;
-		}
-		for(int i = 2; i*i <= p; i++) {
-			//cout << "dividing " << k << " by " << i << endl;
-			if (k % i == 0) {
-				//cerr << i << " divides " << k << endl;
-				isPrime = false;
-				break;
+int main() {
+	long long p, y;
+	cin >> p >> y;
+	
+	long long ans = y;
+	bool prime = true;
+	
+	if (p < 300) {
+		while (ans > p) {
+			for(long long i = 2; i <= p; i++) {
+				if (ans % i == 0) {
+					prime = false;
+					break;
+				}
 			}
-			isPrime = true;
+			if (!prime) {
+				ans--;
+				prime = true;
+			}
+			else {
+				cout << ans << endl;
+				return 0;
+			}
 		}
-		if (isPrime) {
-			cout << k << endl;
-			return 0;
-		}
-		if (k == n-1-300) {
-			cout << -1 << endl;
-			return 0;
-		}
-	}
+		cout << -1 << endl;
+		return 0;
 	}
 	else {
-		for(int k = n; k >= p; k--) {
-			if (k == p) {
+		while (ans > p) {
+			for(long long i = 2; i*i <= y; i++) {
+				if (ans % i == 0 && i != ans && i <= p) {
+					prime = false;
+					break;
+				}
+			}
+			if (ans == y-300) {
 				cout << -1 << endl;
 				return 0;
 			}
-			for(int i = 2; i <= p; i++) {
-				if (k % i == 0) {
-					isPrime = false;
-					break;
-				}
-				isPrime = true;
+			if (!prime) {
+				ans--;
+				prime = true;
 			}
-
-			if (isPrime) {
-				cout << k << endl;
+			else {
+				cout << ans << endl;
 				return 0;
 			}
+			
 		}
-		
+		cout << -1 << endl;
+		return 0;
 	}
-    return 0;
+	
+	return 0;
+	
 }
