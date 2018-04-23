36d0ce158424dfe56459cc74b255e0859b199caa
evalir.io <hi.evalir@gmail.com>
2018-04-23 00:46:30 -0400

    Reorganize

---
+++ Problems/Codeforces/A199.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    unsigned long long n;
+    cin >> n;
+    cout << 0 << ' ' << 0 << ' ' << n << endl;
+    return 0;
+    
+}
--- class.md
+++
@@ -1,8 +0,0 @@
-#TO do
-    Creacion de funciones en C
-    Prototipaje
-    Parametros formales de C
-    Procedimientos
-    Estructuras cicliclas while, for, do
-    Como representar ciclos en diagramas de flujos
-    829-717-9198
