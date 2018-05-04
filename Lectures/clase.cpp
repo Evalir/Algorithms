90e49e70cc42251f8a04a8395679b0b8078ace71
evalir.io <hi.evalir@gmail.com>
2018-05-04 02:55:22 -0400

    Add knapsack solutions

---
+++ Lectures/clase.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    int n = 5;
+    n = n % 3;
+    cout << n << endl;
+    return 0;
+}
