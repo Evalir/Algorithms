372845259a5558f722d641bcfc5c915f22d98f41
Evalir <hi.evalir@gmail.com>
2018-07-10 15:33:50 -0400

    add stuff

--- Problems/spoj/CZPROB1.cpp
+++ Problems/spoj/CZPROB1.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 using namespace std;
 
-int dp[6][8000];
+int dp[8000][6];
 int t,n,k;
 
 int joa(int sum, int ch) {
@@ -12,22 +12,22 @@ int joa(int sum, int ch) {
 		return 0;
 	if (sum == 0)
 		return 1;
-	if (dp[ch][sum] != -1)
-		return dp[ch][sum];
+	if (dp[sum][ch] != -1)
+		return dp[sum][ch];
 
 	int ans = 0;
 	//use num and ++
 	for(int i = 1; i <= k; i++)
 		ans = joa(sum-i, i);
-	dp[ch][sum] = ans;
+	dp[sum][ch] = ans;
 	return ans;
 }
 
 int main() {
 	cin >> t;
 	while(t--) {
-			for(int j = 0; j <= 5; j++) {
-            	for(int z = 0; z <= 7997; z++) {
+			for(int j = 0; j <= 7997; j++) {
+            	for(int z = 0; z <= 5; z++) {
             	 	dp[j][z] = -1;
             	}
         	}
