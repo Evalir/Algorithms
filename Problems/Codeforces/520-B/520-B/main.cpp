c084b8f970300e2228f621e8f6f2319bd66b7747
evalir.io <hi.evalir@gmail.com>
2018-09-09 01:13:42 -0400

    Add digit dp solutions

--- Problems/Codeforces/520-B/520-B/main.cpp
+++ Problems/Codeforces/520-B/520-B/main.cpp
@@ -1,16 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
-
+const int INF = 1 << 30;
 int main() {
     cin >> n >> m;
-    int cnt = 0;
-    while(n < m) {
-        n *= 2;
-        cnt++;
+    vector<int> d((int)1e5 + 10, INF);
+    queue<int> Q;
+    d[n] = 0;
+    Q.push(n);
+    int node, ans;
+    while(!Q.empty()) {
+        node = Q.front(); Q.pop();
+        if (node * 2 <= (int)1e5) {
+            if (d[node*2] == INF) {
+                d[node*2] = d[node] + 1;
+                Q.push(node * 2);
+            }
+        }
+        if (node - 1 > 0) {
+            if (d[node - 1] == INF) {
+                d[node - 1] = d[node] + 1;
+                Q.push(node - 1);
+            }
+        }
+        if (node == m) ans = d[node];
     }
-    cnt += abs(n-m);
-    
-    cout << cnt << endl;
+    cout << ans << endl;
     return 0;
 }
